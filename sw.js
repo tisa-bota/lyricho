9bb7d7c56b82c9b26bea7ab6d594893371e37c99
tisa-bota <tisa-bota@users.noreply.github.com>
2026-04-29 08:02:29 +0000

    deploy: 98528c3e3d5d320fa396056bb8c5d561b54d6682

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION    = '2c95f6c4'; // GitHub Actions がここをコミットSHAに置換
+const VERSION    = '98528c3e'; // GitHub Actions がここをコミットSHAに置換
 const CACHE_NAME = `lyricho-v${VERSION}`;
 
 const ASSETS = [
